a1fce3da5b7ff11db74ced6b5712105da5909021
oscarmiticanoy <oscarmiticanoy@gmail.com>
2018-06-20 14:50:20 -0500

    start proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { EmployComponent } from './employ/employ.component';
+import { ProjectComponent } from './project/project.component';
+import { NoFoundComponent } from './no-found/no-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SidenavComponent,
+    EmployComponent,
+    ProjectComponent,
+    NoFoundComponent
   ],
   imports: [
     BrowserModule
